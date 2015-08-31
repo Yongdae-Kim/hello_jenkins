f8e1fe2852a05fd65e026b30182006ec093e4784
daum <daum@aptest.local>
2015-08-31 14:59:30 +0900

    업데이트

--- newtimer.groovy
+++ newtimer.groovy
@@ -1,23 +1,3 @@
-// File: newtimer.groovy
-class GroovyTimerTask extends TimerTask {
-    Closure closure
-    void run() {
-        closure()
-    }
-}
-
-class TimerMethods {
-    static TimerTask runEvery(Timer timer, long delay, long period, Closure codeToRun) {
-        TimerTask task = new GroovyTimerTask(closure: codeToRun)
-        timer.schedule task, delay, period
-        task
-    }
-}
-
-use (TimerMethods) {
-    def timer = new Timer()
-    def task = timer.runEvery(100, 500) {
-        println "Task executed at ${new Date()}."
-    }
-    println "Current date is ${new Date()}."
-}
+def timer = new Timer()
+def task = timer.runAfter(3000) { println "Actually executed at ${new Date()}." }
+println "Current date is ${new Date()}. Task is executed at ${new Date(task.scheduledExecutionTime())}."
